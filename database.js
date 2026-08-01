9afd5ce56c6f4eb357850e709e569e3f15396bcf
gcoviello09-dot <g.coviello09@icloud.com>
2026-08-01 04:38:19 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,346 +1,291 @@
 const missionData = {
 
 "Iron Man":{
-anno:"2008",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "The Incredible Hulk":{
-anno:"2008",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Iron Man 2":{
-anno:"2010",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Thor":{
-anno:"2011",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Avengers":{
-anno:"2012",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Captain America: The First Avenger":{
-anno:"2011",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Iron Man 3":{
-anno:"2013",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Thor: The Dark World":{
-anno:"2013",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Captain America: The Winter Soldier":{
-anno:"2014",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Guardians of the Galaxy":{
-anno:"2014",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Avengers: Age of Ultron":{
-anno:"2015",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Ant-Man":{
-anno:"2015",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Captain America: Civil War":{
-anno:"2016",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Black Panther":{
-anno:"2018",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Doctor Strange":{
-anno:"2016",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Thor Ragnarok":{
-anno:"2017",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Guardians of the Galaxy Vol.2":{
-anno:"2017",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Spider-Man Homecoming":{
-anno:"2017",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Ant-Man and The Wasp":{
-anno:"2018",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Avengers Infinity War":{
-anno:"2018",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Avengers Endgame":{
-anno:"2019",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Spider-Man Far From Home":{
-anno:"2019",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Spider-Man No Way Home":{
-anno:"2021",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Thor Love and Thunder":{
-anno:"2022",
 tipo:"Film",
 saga:"Infinity Saga"
 },
 
 "Black Panther Wakanda Forever":{
-anno:"2022",
 tipo:"Film",
 saga:"Infinity Saga"
 },
+
+
 "Loki":{
-anno:"2021",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "What If...?":{
-anno:"2021",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "WandaVision":{
-anno:"2021",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Shang-Chi":{
-anno:"2021",
 tipo:"Film",
 saga:"Multiverse Saga"
 },
 
 "The Falcon and the Winter Soldier":{
-anno:"2021",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Eternals":{
-anno:"2021",
 tipo:"Film",
 saga:"Multiverse Saga"
 },
 
 "Doctor Strange Multiverse of Madness":{
-anno:"2022",
 tipo:"Film",
 saga:"Multiverse Saga"
 },
 
 "Hawkeye":{
-anno:"2021",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Moon Knight":{
-anno:"2022",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "I Am Groot":{
-anno:"2022",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "She-Hulk":{
-anno:"2022",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Ms Marvel":{
-anno:"2022",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Werewolf by Night":{
-anno:"2022",
 tipo:"Special",
 saga:"Multiverse Saga"
 },
 
 "Guardians Holiday Special":{
-anno:"2022",
 tipo:"Special",
 saga:"Multiverse Saga"
 },
 
 "Ant-Man Quantumania":{
-anno:"2023",
 tipo:"Film",
 saga:"Multiverse Saga"
 },
 
 "Secret Invasion":{
-anno:"2023",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Guardians of the Galaxy Vol.3":{
-anno:"2023",
 tipo:"Film",
 saga:"Multiverse Saga"
 },
 
 "The Marvels":{
-anno:"2023",
 tipo:"Film",
 saga:"Multiverse Saga"
 },
 
 "Echo":{
-anno:"2024",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Deadpool & Wolverine":{
-anno:"2024",
 tipo:"Film",
 saga:"Multiverse Saga"
 },
 
 "Agatha All Along":{
-anno:"2024",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Daredevil Born Again":{
-anno:"2024",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Ironheart":{
-anno:"2025",
 tipo:"Serie",
 saga:"Multiverse Saga"
 },
 
 "Captain America Brave New World":{
-anno:"2025",
 tipo:"Film",
 saga:"Multiverse Saga"
 },
 
 "Thunderbolts":{
-anno:"2025",
 tipo:"Film",
 saga:"Multiverse Saga"
 },
 
+
 "Jessica Jones":{
-anno:"2015",
 tipo:"Serie",
 saga:"Marvel Television"
 },
 
 "Luke Cage":{
-anno:"2016",
 tipo:"Serie",
 saga:"Marvel Television"
 },
 
 "Iron Fist":{
-anno:"2017",
 tipo:"Serie",
 saga:"Marvel Television"
 },
 
 "The Defenders":{
-anno:"2017",
 tipo:"Serie",
 saga:"Marvel Television"
 },
 
 "The Punisher":{
-anno:"2017",
 tipo:"Serie",
 saga:"Marvel Television"
 },
 
 
 "Wonder Man":{
-anno:"In arrivo",
 tipo:"Serie",
 saga:"Coming Soon"
 },
 
 "Vision Quest":{
-anno:"In arrivo",
 tipo:"Serie",
 saga:"Coming Soon"
-
 }
 
 };
