b406253aa3d52ffce9829b341b80f1de23c7094d
gcoviello09-dot <g.coviello09@icloud.com>
2026-08-01 04:12:06 +0200

    Create database.js

---
+++ database.js
@@ -0,0 +1,21 @@
+const missionData = {
+
+"Iron Man":{
+anno:"2008",
+tipo:"Film",
+saga:"Infinity Saga"
+},
+
+"The Incredible Hulk":{
+anno:"2008",
+tipo:"Film",
+saga:"Infinity Saga"
+},
+
+"Iron Man 2":{
+anno:"2010",
+tipo:"Film",
+saga:"Infinity Saga"
+}
+
+};
